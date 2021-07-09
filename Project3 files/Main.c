a9698577534c118390f3876d653f989e060faadc
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2021-07-09 20:58:51 +0300

    1st Commit
    First commit
    -Included helping files
    -Basic main structure
    - First take on To-Do List

---
+++ Project3 files/Main.c
@@ -0,0 +1,19 @@
+/* Argyros Konstantinos
+AM: 2022202000014
+dit20014@go.uop.gr
+
+Pavlos Sygrimis
+AM: 2022202000202
+dit20202@go.uop.gr */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "bmp.h"
+
+// 3 main parameters (command, input_file, output_file)
+int main (int argc, char *argv[])
+{
+    
+
+    return 0;
+}
