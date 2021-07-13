dc0f32d7ddd149c4811920f360388f9ddc38fab7
SILVERARGYROS <76134085+SILVERARGYROS@users.noreply.github.com>
2021-07-13 16:18:27 +0300

    Understanding the concept
    - Added Makefile
    - Renamed Main.c to main.c for simplicity
    - Fixed array reading bug
    - 2nd file is now read ONLY IF INPUT != "-a"

--- Project3 files/functions.c
+++ Project3 files/functions.c
@@ -1,31 +1,42 @@
+/* Argyros Konstantinos
+AM: 2022202000014
+dit20014@go.uop.gr
+
+Pavlos Sygrimis
+AM: 2022202000202
+dit20202@go.uop.gr */
+
 //foo.c
 
 #include "bmp.h"
 
 // prototypes
-void freearrays (rgb** pavlos, rgb** sygrimis, int x)
+
+void freearrays (rgbe** pavlos, rgbe** sygrimis, int x)
 {
+    int i, j;
     for(i = 0; i < x; i++)
     {
         free(pavlos[i]);
     }
-    free(pavlos)
+    free(pavlos);
 
     for(i = 0; i < x; i++)
     {
         free(sygrimis[i]);
     }
-    free(sygrimis)
+    free(sygrimis);
 }
 
-void printattributes (bmp_header *h1, bmp_info_header *h2)
+void printattributes (struct bmp_header *h1,struct bmp_info *h2)
 {
     // prints all struct attributes
-
+    printf("DEBUG MESSAGE: successful so far\n");
 }
 
-void flip (rgb** pavlos, rgb** sygrimis, int x, int y, char lever)
+void flip (rgbe** pavlos, rgbe** sygrimis, int x, int y, char lever)
 {
+    int i, j;
     for(i = 0; i < x; i++)
     {
         for(j = 0; j < y; j++)
@@ -42,14 +53,14 @@ void flip (rgb** pavlos, rgb** sygrimis, int x, int y, char lever)
     }
 }
 
-void rgrey (rgb** pavlos, rgb** sygrimis, int x, int y)
+void rgrey (rgbe** pavlos, rgbe** sygrimis, int x, int y)
 {
-    int sum;
+    int sum, i, j;
     for(i = 0; i < x; i++)
     {
         for(j = 0; j < y; j++)
         {
-            sum = (pavlos[i][j].red + pavlos[i][j].green + pavlos[i][j].blue)/3
+            sum = (pavlos[i][j].red + pavlos[i][j].green + pavlos[i][j].blue)/3;
             sygrimis[i][j].red = sum;
             sygrimis[i][j].green = sum;
             sygrimis[i][j].blue = sum;
@@ -57,8 +68,9 @@ void rgrey (rgb** pavlos, rgb** sygrimis, int x, int y)
     }
 }
 
-void agrey (rgb** pavlos, rgb** sygrimis, int x, int y, int red, int green, int blue, int percentage)
+void agrey (rgbe** pavlos, rgbe** sygrimis, int x, int y, int red, int green, int blue, int percentage)
 {
+    int i, j;
     rgrey(pavlos, sygrimis, x, y);
     
 }
